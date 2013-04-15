f650c8db4d7ee5257d9c121424f1bf902152c301
Johan Henkens <johan@henkens.com>
2013-04-14 17:45:37 -0700

    Initial Commit

---
+++ Crypto Homework 1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Crypto Homework 1
+//
+//  Created by Johan Henkens on 4/14/13.
+//  Copyright (c) 2013 Johan Henkens. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
